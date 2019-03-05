822f997e23bd2e92b07bd52f687159a702886c2b
Michał Sądel <michal.sadel@ista.pl>
2019-03-05 14:50:49 +0100

    Initial application setup

---
+++ src/main/java/com/michalsadel/ecar/Application.java
@@ -0,0 +1,11 @@
+package com.michalsadel.ecar;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
