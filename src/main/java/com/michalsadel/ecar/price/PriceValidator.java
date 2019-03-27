dfb0ad2b2a799f034d7a665e8033bb86b1f149aa
Michal Sadel <michal.sadel@ista.com>
2019-03-27 16:23:42 +0100

    Just forgot interface in composite

--- src/main/java/com/michalsadel/ecar/price/PriceValidator.java
+++ src/main/java/com/michalsadel/ecar/price/PriceValidator.java
@@ -3,15 +3,15 @@ package com.michalsadel.ecar.price;
 import java.util.HashSet;
 import java.util.Set;
 
-class PriceValidator {
+class PriceValidator implements Validator {
     private final Set<Validator> validators = new HashSet<>();
 
     void addValidator(Validator validator) {
         validators.add(validator);
     }
 
-    void validate(Price incomingPrice) {
+    @Override
+    public void validate(Price incomingPrice) {
         validators.forEach(validator -> validator.validate(incomingPrice));
     }
-
 }
